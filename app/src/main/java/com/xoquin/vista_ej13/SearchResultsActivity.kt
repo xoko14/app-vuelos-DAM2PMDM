5615ce52060f3e582df75d9a3a548ceb2d785847
xoko14 <xoquinperezb@gmail.com>
2021-11-17 18:00:09 +0100

    fin clase 17/11/2021

--- app/src/main/java/com/xoquin/vista_ej13/SearchResultsActivity.kt
+++ app/src/main/java/com/xoquin/vista_ej13/SearchResultsActivity.kt
@@ -2,10 +2,18 @@ package com.xoquin.vista_ej13
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.xoquin.vista_ej13.vo.BusquedaVuelo
+import com.xoquin.vista_ej13.vo.Vuelo
 
 class SearchResultsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_results)
+
+        intent.getSerializableExtra("")
+    }
+
+    fun buscar(busquedaVuelo: BusquedaVuelo): List<Vuelo>{
+        TODO()
     }
 }
---
+++ app/src/main/java/com/xoquin/vista_ej13/dao/VueloDAO.kt
@@ -0,0 +1,13 @@
+package com.xoquin.vista_ej13.dao
+
+import com.xoquin.vista_ej13.vo.Vuelo
+
+class VueloDAO: DAO<Vuelo> {
+    override fun getOne(): Vuelo {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAll(): List<Vuelo> {
+        TODO("Not yet implemented")
+    }
+}
